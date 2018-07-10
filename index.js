3f3cc9677dc02902edf1ffa84cf8ac7cf4cc090f
Phil Wells <phil.wells@nytimes.com>
2018-07-10 16:21:06 -0400

    multiple rooms

--- index.js
+++ index.js
@@ -11,10 +11,33 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
 })
 
-io.on('connection', (socket) => {
-  console.log('user connected')
-  socket.on('message', (msg) => {
-    console.log(`message: ${msg}`)
-    io.emit('message', msg)
+app.get('/javascript', (req, res) => {
+  res.sendFile(__dirname + '/public/javascript.html')
+})
+
+app.get('/swift', (req, res) => {
+  res.sendFile(__dirname + '/public/swift.html')
+})
+
+app.get('/css', (req, res) => {
+  res.sendFile(__dirname + '/public/css.html')
+})
+
+// tech namespace
+const tech = io.of('/tech')
+
+tech.on('connection', (socket) => {
+  socket.on('join', (data) => {
+    socket.join(data.room)
+    tech.in(data.room).emit('message', `New user joined ${data.room}!`)
+  })
+
+  socket.on('message', (data) => {
+    console.log(`message: ${data.msg}`)
+    tech.in(data.room).emit('message', data.msg)
+  })
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+    tech.emit('message', 'user disconnected')
   })
 })
